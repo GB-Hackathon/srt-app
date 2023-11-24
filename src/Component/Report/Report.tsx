5c88488f3e4a9cb975aefcb606dfe9e46d3565e6
dya-only <dyacode@proton.me>
2023-11-25 02:35:26 +0900

    feat: discord webhook

--- src/Component/Report/Report.tsx
+++ src/Component/Report/Report.tsx
@@ -1,50 +1,62 @@
-import { useCallback, useRef } from "react";
-import "./Report.css"
-import Webcam from "react-webcam"
-import axios from "axios";
+import { useCallback, useRef } from 'react'
+import './Report.css'
+import Webcam from 'react-webcam'
+import axios from 'axios'
+import { Webhook, MessageBuilder } from 'discord-webhook-node'
 
 const Report: React.FC = () => {
   const webcam: any = useRef()
+  const hook = new Webhook('https://discord.com/api/webhooks/1177657247300595774/jaEUc1iyGa2oCpPDZiym4gJMmaxI_Gu-d8yVyu7-zQsWdOwdxMHIHxovJhhuAkVSftUT')
   let constraints = {
-    facingMode: { exact: "user" },
+    facingMode: { exact: 'user' },
   }
 
-  const getCapture = useCallback(
-    () => {
-      const img = webcam.current.getScreenshot()
-      console.log(img)
+  const getCapture = useCallback(() => {
+    const img = webcam.current.getScreenshot()
+    console.log(img)
 
-      axios.post('/eme', { image: img.split(',')[1] }, {
-        headers: {
-          'Content-Type': 'application/json'
+    axios
+      .post(
+        '/eme',
+        { image: img.split(',')[1] },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+          },
         }
-      }).then(resp => {
+      )
+      .then((resp) => {
         console.log(resp)
+
+        const embed = new MessageBuilder()
+          .setTitle('성민이 발생 신고')
+          .setAuthor('바보성민')
+          .addField('경소고 기숙사', '바보 성민', true)
+          .setColor('#00b0f4')
+          .setThumbnail('https://cdn.discordapp.com/embed/avatars/0.png')
+          // .setDescription('Oh look a description :)')
+          // .setImage('https://cdn.discordapp.com/embed/avatars/0.png')
+          // .setFooter('Hey its a footer', 'https://cdn.discordapp.com/embed/avatars/0.png')
+          .setTimestamp()
+
+        hook.send(embed)
       })
-    },
-    [webcam]
-  )
+  }, [webcam])
 
   return (
-    <div className="report_inner">
-      <div className="report_cam-container">
-        <div className="report_cambox">
-          <Webcam
-            videoConstraints={constraints}
-            mirrored={true}
-            disablePictureInPicture={true}
-            className="webcam"
-            ref={webcam}
-          />
+    <div className='report_inner'>
+      <div className='report_cam-container'>
+        <div className='report_cambox'>
+          <Webcam videoConstraints={constraints} mirrored={true} disablePictureInPicture={true} className='webcam' ref={webcam} />
         </div>
-        <div className="report_btnbox">
-          <div className="report_album" />
-          <button className="report_submit" onClick={getCapture}></button>
-          <div className="report_switch-camera" />
+        <div className='report_btnbox'>
+          <div className='report_album' />
+          <button className='report_submit' onClick={getCapture}></button>
+          <div className='report_switch-camera' />
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
 export default Report
